9258b3004f8608e7729475ffb35c55bafbdad2cc
k8Howard <sweetk8@me.com>
2018-07-10 18:28:28 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz (your_number)
+  if your_number % 3== 0 && your_number % 5 == 0
+    "FizzBuzz"
+  elsif your_number % 3 == 0 
+    "Fizz"
+  elsif your_number % 5 == 0 
+    "Buzz"
+  else
+  end
+end
